d28e4f0c629f6f9dcda4345b1673a8859b155687
polezhaevr <mrboogie8995@gmail.com>
2021-04-03 23:55:06 +0500

    add value var and console output

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 
-let money,
-    income, 
-    addExpenses, 
-    deposit,
-    mission,
-    period ; 
+let money = 60000,
+    income = "СТО", 
+    addExpenses = "МУЗЫКА , КОММУНАЛКА , ИНТЕРНЕТ", 
+    deposit = true,
+    mission = 500000,
+    period = 12 , 
+    budgetDay = money/30;  
 
-    alert ("Hello world");
-    console.log ("Hello world");
+    console.log(typeof money + "\n" + typeof income + "\n" + typeof deposit);
+    console.log(addExpenses.length);
+    console.log("Период равен " + period + " месяцев" + "\n" + "Цель заработать " + mission + " рублей") ;
+    console.log(addExpenses.toLowerCase().split(','));
+    console.log(budgetDay + "руб");
+
+    
+
+    
 
 
 
